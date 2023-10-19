f0f48c3672559260bdb44ef276a3b1ca1b7fe031
lerocha1 <38813827+lerocha1@users.noreply.github.com>
2023-10-19 13:29:42 -0300

    ramon eu te amo

--- filme/urls.py
+++ filme/urls.py
@@ -1,10 +1,11 @@
 #para cada pagina precisa de 3 coisas
 #url - view - template --- Sempre essas 3 coisas!
 from django.urls import path
-from .views import  homefilmes, Homepage
+from .views import  Homefilmes, Homepage, Seriefilmes
 
 
 urlpatterns = [
     path('', Homepage.as_view()),
-    path('filmes', homefilmes ),
+    path('filmes', Homefilmes.as_view() ),
+    path('serie', Seriefilmes.as_view()),
     ]
--- filme/views.py
+++ filme/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Filme
-from django.views.generic import TemplateView
+from .models import Filme, Seriado
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
 
@@ -10,9 +10,17 @@ from django.views.generic import TemplateView
 class Homepage(TemplateView):
     template_name = 'homepage.html'
 
-
+"""
 def homefilmes(request):
     context = {}
     lista_filmes = Filme.objects.all #aqui pegou todos os filmes do banco de dados usando a classe Filme
     context['lista_filmes'] = lista_filmes
     return render(request, "homefilmes.html", context)
+"""
+class Homefilmes(ListView):
+    template_name = "homefilmes.html"
+    model = Filme #quando usa class para uma lista o padrão é object_lis, precisa trocar no html do filmes
+
+class Seriefilmes(ListView):
+    template_name = "seriefilmes.html"
+    model = Seriado

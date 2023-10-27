c9c8876f4bad70acdfc05867e3ab0659de1c8790
lerocha1 <38813827+lerocha1@users.noreply.github.com>
2023-10-27 11:29:04 -0300

    Ajustando models e banco com a classe episodio

--- filme/models.py
+++ filme/models.py
@@ -54,3 +54,6 @@ class Episodio(models.Model):
     filme = models.ForeignKey("Filme", related_name='episodios', on_delete=models.CASCADE)
     titulo = models.CharField(max_length=100)
     video = models.URLField()
+
+    def __str__(self):
+        return self.filme.titulo + " - " + self.titulo
